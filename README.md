40307fec82ef9dec7d67c5303f3e5e8c06de2f6f
cubbic <danpopa1998@gmail.com>
2016-08-23 04:53:09 +0300

    terminat scriptul , ramas de adaugat documentatia

---
+++ README.md
@@ -0,0 +1,1 @@
+YouTube Data API does not provide a possibility to get a random video. This module aims to solve that problem.
---
+++ test.js
@@ -0,0 +1,5 @@
+var youtube = require('./index.js');
+
+youtube.getRandomVid('AIzaSyCeoBW236__aaWnHt2ERrwyZhhE0otBLPw', function(err , obj){
+  console.log(obj);
+})
